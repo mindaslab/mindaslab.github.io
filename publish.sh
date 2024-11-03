2951c017f51e3bc106ffd86cab5884aa21be6c9e
Karthikeyan A K <mindaslab@protonmail.com>
2024-11-03 08:53:17 +0530

    blog updated

--- publish.sh
+++ publish.sh
@@ -1,8 +1,12 @@
+git fetch
+git pull origin master
 git add -A
 git commit -am "blog updated"
 git push origin master
 bundle exec jekyll build
 cd ../blog_output
+git fetch
+git pull origin master
 git add -A
 git commit -am "blog updated"
 git push origin master
